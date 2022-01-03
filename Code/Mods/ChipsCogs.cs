8677d41424a7c5ba35b152f84a933e118c774367
Vheos <Vheos777@gmail.com>
2022-01-03 15:43:10 +0100

    cleaned up usings

--- Code/Mods/ChipsCogs.cs
+++ Code/Mods/ChipsCogs.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.Linq;
-    using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.SceneManagement;
     using HarmonyLib;
     using Tools.ModdingCore;
     using Tools.Extensions.Math;
